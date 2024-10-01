37b08b0b6374c3eafa2bc8a5dc239c8927bb8761
debasish218 <debasishmahapatra218@gmail.com>
2024-10-01 20:53:04 +0530

    first

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import render_template
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello', methods=['GET'])
 def hello():
-    return "Hello World !"
+    return "Hello World !", 200
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
